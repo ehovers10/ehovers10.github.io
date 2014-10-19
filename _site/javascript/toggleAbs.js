b93e4810cce2dd8c051b4cf70e5736e2d7475da7
erik hoversten <ehovers10@gmail.com>
2014-10-19 09:45:12 -0400

    update with paper abstracts

--- _site/javascript/toggleAbs.js
+++ _site/javascript/toggleAbs.js
@@ -2,6 +2,10 @@ function toggleAbs(paper) {
 	
 	var abs = document.getElementById('abs_'+paper);
 
-	abs.className.indexOf('noshow') == -1?abs.className = 'noshow':abs.className = 'show';
+	abs.className.indexOf('noshow') == -1?abs.className = 'noshow':abs.className = 'abstract';
+	
+	var title = document.getElementById('title_'+paper);
+	
+	title.className.indexOf('noshow') == -1?title.className = 'noshow':title.className = 'show';
 
 }
--- research/semantics-bib.md
+++
@@ -1,9 +0,0 @@
----
-layout: elay1
-title: semantics bibliography
----
-
-## {{ page.title }}
-
-
-{% bibliography %}
