8232bc3ef603b04c75c1cd510af9ca254e58e858
Erik Hoversten <ehovers10@gmail.com>
2015-03-19 09:22:07 -0400

    modern cogito collection

--- _site/javascript/scroll-fix.js
+++ _site/javascript/scroll-fix.js
@@ -8,4 +8,21 @@ $(document).ready(function(){
 				 $("#bibtitle").removeClass('fixed');
 			 }
 		});
-	});
+
+$('.lightbox').bind('mousewheel DOMMouseScroll', function(e) {
+    var scrollTo = null;
+
+    if (e.type == 'mousewheel') {
+        scrollTo = (e.originalEvent.wheelDelta * -1);
+    }
+    else if (e.type == 'DOMMouseScroll') {
+        scrollTo = 40 * e.originalEvent.detail;
+    }
+
+    if (scrollTo) {
+        e.preventDefault();
+        $(this).scrollTop(scrollTo + $(this).scrollTop());
+    }
+  });
+
+});
